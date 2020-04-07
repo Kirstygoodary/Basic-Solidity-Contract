ce8aa4f32272d6ade4da15030f2b505df8b686fa
Kirsty Goodary <kirstygoodary1@gmail.com>
2020-04-07 11:49:41 +0100

    added compile.js

--- compile.js
+++ compile.js
@@ -1,16 +1,16 @@
-const path = require("path"); //builds a directory path from the file in to Inbox.sol file. Cross-platform compatibility
-const fs = require("fs"); // file system module
-const solc = require("solc");
+const path = require('path') //builds a directory path from the file in to Inbox.sol file. Cross-platform compatibility
+const fs = require('fs') // file system module
+const solc = require('solc')
 
-const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
-/* __dirname - constant defined by node. takes you from route directory to Inbox.sol
+const lotteryPath = path.resolve(__dirname, 'contracts', 'Lottery.sol')
+/* __dirname - constant defined by node. takes you from route directory to Lottery.sol
  */
 
-const source = fs.readFileSync(inboxPath, "utf8");
+const source = fs.readFileSync(lotteryPath, 'utf8')
 /* reading raw source code from contract. 
  pass in inboxPath 
  utf8 - type of encoding
 */
 
-module.exports = solc.compile(source, 1).contracts[":Inbox"];
+module.exports = solc.compile(source, 1).contracts[':Lottery']
 // pass in source code, and specify the number of difference contracts we are compiling (here its 1)
